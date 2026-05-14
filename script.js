fd5d3c2764afc110c474ca8c9fb6f9055272adfa
vikram kumar <vikram.67453@iqra.edu.pk>
2026-05-15 03:55:36 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('loginForm').addEventListener('submit', function(e) {
-  e.preventDefault();
-  const btn = document.querySelector('.login-btn');
-  btn.textContent = 'Logging in...';
-  btn.disabled = true;
-  setTimeout(() => {
-    window.location.href = 'awareness.html';
-  }, 1500);
-});
